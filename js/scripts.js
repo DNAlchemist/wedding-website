48bd092550065d568c26d0eaf10bcbaa0d01581b
Mikhalev Ruslan <DNAlchemist@ya.ru>
2019-04-24 23:06:05 +0000

    Create google calendar

--- js/scripts.js
+++ js/scripts.js
@@ -201,23 +201,23 @@ $(document).ready(function () {
         },
         data: {
             // Event title
-            title: "Ram and Antara's Wedding",
+            title: "Свадьба Руслана и Анастасии",
 
             // Event start date
-            start: new Date('Nov 27, 2017 10:00'),
+            start: new Date('Jul 20, 2019 10:00'),
 
             // Event duration (IN MINUTES)
             // duration: 120,
 
             // You can also choose to set an end time
             // If an end time is set, this will take precedence over duration
-            end: new Date('Nov 29, 2017 00:00'),
+            end: new Date('Jul 22, 2019 00:00'),
 
             // Event Address
-            address: 'ITC Fortune Park Hotel, Kolkata',
+            address: 'Россия, Новосибирская область, Садовый, ул. Березовая 14/7',
 
             // Event Description
-            description: "We can't wait to see you on our big day. For any queries or issues contact Mr. Amit Roy at +91 9435021804 or +91 7086018971."
+            description: "Мы ждем Вас в этот знаменательный день. По всем вопросам звоните: +79133791299 (Ирина)"
         }
     });
 

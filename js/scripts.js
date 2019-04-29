52035f512cdbf50614063368aa99e77659151d85
Mikhalev Ruslan <DNAlchemist@ya.ru>
2019-04-29 08:19:42 +0000

    Remove google map feature

--- js/scripts.js
+++ js/scripts.js
@@ -257,35 +257,6 @@ $(document).ready(function () {
 
 /********************** Extras **********************/
 
-// Google map
-function initMap() {
-    var itc_kol = {lat: 22.5932759, lng: 88.27027720000001};
-    var map = new google.maps.Map(document.getElementById('map-canvas'), {
-        zoom: 15,
-        center: itc_kol,
-        scrollwheel: false
-    });
-
-    var marker = new google.maps.Marker({
-        position: itc_kol,
-        map: map
-    });
-}
-
-function initBBSRMap() {
-    var la_fiesta = {lat: 20.305826, lng: 85.85480189999998};
-    var map = new google.maps.Map(document.getElementById('map-canvas'), {
-        zoom: 15,
-        center: la_fiesta,
-        scrollwheel: false
-    });
-
-    var marker = new google.maps.Marker({
-        position: la_fiesta,
-        map: map
-    });
-}
-
 function fillByCode(code) {
     $.getJSON("/api/guests/" + code, {}, function(data) {
         $('#name').val(data.name);
